6b28940556d9f8b35aff881b53be3a82f5178579
ceofred <johnsonmessilo19@gmail.com>
2019-07-04 21:54:33 +0100

    psq ssl

--- app/config/config.js
+++ app/config/config.js
@@ -12,6 +12,8 @@ const config = {
       database: 'wayfarer',
       password: 'iftrueconnect',
       port: '5432',
+      ssl: false,
+
     },
   },
 
@@ -26,6 +28,8 @@ const config = {
       database: 'wayfarer_test',
       password: 'iftrueconnect',
       port: '5432',
+      ssl: false,
+
     },
   },
 
@@ -40,6 +44,8 @@ const config = {
       database: 'df6k9h555q07ar',
       password: '892b54e56f50fa41ea695d10f5f5fd49dd42685f74fe482e6089aaca7ce7d270',
       port: '5432',
+      ssl: true,
+
     },
   },
   ci: {
@@ -48,11 +54,12 @@ const config = {
       hostname: process.env.HOSTNAME || 'localhost',
     },
     database: {
-      user: 'codemon',
-      host: process.env.PGHOST,
-      database: 'wayfarer_test',
-      password: 'iftrueconnect',
-      port: process.env.PGPORT,
+      user: 'iyfuthdxraayah',
+      host: 'ec2-174-129-29-101.compute-1.amazonaws.com',
+      database: 'df6k9h555q07ar',
+      password: '892b54e56f50fa41ea695d10f5f5fd49dd42685f74fe482e6089aaca7ce7d270',
+      port: '5432',
+      ssl: true,
     },
   },
 };
